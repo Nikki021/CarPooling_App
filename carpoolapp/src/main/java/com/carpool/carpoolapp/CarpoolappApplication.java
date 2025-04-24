eed2bdded45b8fcdd2bfac15a56dd7364189aa9e
Nikhil Matcha <nikhil.matcha021@gmail.com>
2025-04-24 14:41:29 -0500

    updated

---
+++ carpoolapp/src/main/java/com/carpool/carpoolapp/CarpoolappApplication.java
@@ -0,0 +1,13 @@
+package com.carpool.carpoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolappApplication.class, args);
+	}
+
+}
---
+++ carpoolapp/src/test/java/com/carpool/carpoolapp/CarpoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpool.carpoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
